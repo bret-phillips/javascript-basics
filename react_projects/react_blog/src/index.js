3873a82d47c9cb71793e8479143c130b7ad0a411
bret-phillips <bret.phillips@rocketmail.com>
2017-10-09 19:18:11 -0600

    fixed react-blog

--- react_projects/react_blog/src/index.js
+++ react_projects/react_blog/src/index.js
@@ -4,15 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends
-
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
